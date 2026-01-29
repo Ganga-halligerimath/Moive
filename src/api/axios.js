53eb9ee3b2cc690eba41729bec15a5cd22b2f5b7
Ganga <gangadevihalligerimath@gmail.com>
2026-01-29 23:25:05 +0530

    Update Axios config for Vercel frontend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,28 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "https://movie-backend-tnrt.onrender.com/api",
+//   withCredentials: true,
+ 
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default api;
+
+
+
 import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://movie-backend-tnrt.onrender.com/api",
-  withCredentials: true,
- 
+  withCredentials: true // only if backend uses cookies
 });
 
 api.interceptors.request.use((config) => {
