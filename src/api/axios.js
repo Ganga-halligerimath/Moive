43a225c96aa436b202d9ef3fe188fe5df604cbec
Ganga <gangadevihalligerimath@gmail.com>
2026-01-29 22:33:22 +0530

    Your commit message describing changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://movie-backend-tnrt.onrender.com/api",
+  withCredentials: true,
+ 
 });
 
 api.interceptors.request.use((config) => {
